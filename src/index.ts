42b77361c6c325c9d94154614409fd2c594e94f8
Max Heilmann <support@matix-media.net>
2021-11-04 21:55:15 +0100

    Fixed formatting

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,21 @@
 import * as dotenv from "dotenv";
 import * as express from "express";
 import * as expressWs from "express-ws";
-import * as cors from "cors"
+import * as cors from "cors";
 import logger from "./logger";
-
 
 dotenv.config();
 
 (async () => {
     const port = process.env.PORT;
     const app = express();
-    
+
     logger.info("Syncing database...");
     // Sequelize setup here
 
     app.use(cors());
     app.use(express.json());
-    app.use(express.urlencoded({extended: true}));
+    app.use(express.urlencoded({ extended: true }));
 
     app.listen(port, () => {
         logger.info("Started. UI Reachable through http://localhost:" + port + "/");
