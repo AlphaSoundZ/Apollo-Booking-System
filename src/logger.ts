96d8c061f2c536b39c7c67c81d6a339db66028ea
Max Heilmann <support@matix-media.net>
2021-11-04 19:41:21 +0100

    Logger config

---
+++ src/logger.ts
@@ -0,0 +1,40 @@
+import log4js from "log4js";
+import path from "path";
+
+
+const devMode = (process.env.NODE_ENV || "development") == "development";
+
+const logPath = path.join(__dirname, "../", "logs");
+const logLevel = devMode ? "debug" : "info";
+
+log4js.configure({
+    appenders: {
+        database: {
+            type: "file",
+            filename: path.join(logPath, "db.log"),
+        },
+        log: {
+            type: "file",
+            filename: path.join(logPath, "log.log"),
+        },
+        console: {
+            type: "stdout",
+        }
+    },
+    categories: {
+        default: {
+            appenders: ["log", "console"],
+            level: logLevel,
+        },
+        server: {
+            appenders: ["log", "console"],
+            level: logLevel,
+        },
+        database: {
+            appenders: ["database"],
+            level: logLevel
+        }
+    }
+});
+
+export default log4js.getLogger("server");
