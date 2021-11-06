8bc88dc02aeb6c77637ba1366fa2f268e05a1e04
Max Heilmann <support@matix-media.net>
2021-11-06 16:06:02 +0100

    Added loading page

---
+++ client/src/views/Loading.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="loading">
+        <BigMessage :header="header" :message="message" icon="spinner-icon" />
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import BigMessage from "@/components/BigMessage.vue";
+
+export default Vue.extend({
+    components: { BigMessage },
+    props: {
+        header: {
+            default: "Bitte warten...",
+        },
+        message: {
+            default: "",
+        },
+    },
+});
+</script>
+
+<style lang="scss" scoped>
+.loading {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+}
+</style>
