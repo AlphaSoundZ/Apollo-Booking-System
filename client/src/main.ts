1fc58c37e14892177759cd9f7c5adb470f2cea44
Max Heilmann <support@matix-media.net>
2021-11-06 16:05:20 +0100

    Included material icons

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,6 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import "@/assets/fonts/Roboto.css";
+import "@/assets/fonts/MaterialIcons.css";
 
 Vue.config.productionTip = false;
 
