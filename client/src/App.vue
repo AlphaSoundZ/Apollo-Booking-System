8752d51e5c15b2d41ff5e0a5a9ce19db90fedbc0
Max Heilmann <support@matix-media.net>
2021-11-06 17:36:30 +0100

    Fixed typing

--- client/src/App.vue
+++ client/src/App.vue
@@ -29,7 +29,7 @@ export default Vue.extend({
         };
         this.hideMouse();
         setTimeout(() => {
-            this.$refs.application.requestFullscreen();
+            (this.$refs.application as Element).requestFullscreen();
         }, 1);
     },
 });
