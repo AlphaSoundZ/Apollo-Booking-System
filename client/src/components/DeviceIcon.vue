beb895d325a77fbcef56cabd36c287544d6511c0
Max Heilmann <support@matix-media.net>
2021-11-06 19:25:24 +0100

    Added device icon

---
+++ client/src/components/DeviceIcon.vue
@@ -0,0 +1,20 @@
+<template>
+    <span class="mi">{{ mapping[deviceType] }}</span>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+    props: {
+        deviceType: String,
+    },
+    data() {
+        return {
+            mapping: {
+                laptop: "laptop",
+                smartphone: "smartphone",
+            },
+        };
+    },
+});
+</script>
