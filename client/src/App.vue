c3d7d6a7354cf4ae4711b4a57a748b8415b2ba1e
Max Heilmann <support@matix-media.net>
2021-11-06 14:48:14 +0100

    Initialized ui vue project

---
+++ client/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+    <div id="app">
+        <div class="branding">
+            <img src="@/assets/img/school-logo.png" alt="" />
+        </div>
+        <router-view />
+    </div>
+</template>
+
+<style lang="scss">
+#app {
+    font-family: Roboto, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+}
+</style>
+
+<style lang="scss" scoped>
+.branding {
+    background-image: url("~@/assets/img/dotted-cricles.svg");
+    background-repeat: no-repeat;
+    position: absolute;
+    width: 867px;
+    height: 867px;
+    top: 0;
+    left: 0;
+
+    img {
+        padding: 22px;
+        max-width: 195px;
+        max-height: 195px;
+    }
+}
+</style>
