a98353fc9d6875f011a0f1aa7def6c4e98d1881f
Max Heilmann <support@matix-media.net>
2021-11-06 17:06:00 +0100

    Added waiting screen

--- client/src/views/Waiting.vue
+++ client/src/views/Waiting.vue
@@ -1,9 +1,38 @@
 <template>
-    <div class="waiting"></div>
+    <div class="waiting centered-page">
+        <div>
+            <BigMessage message="Zum Ausleihen" header="Halte deinen Chip an das Lesegerät">
+                <CombinedIcon>
+                    <span class="mi">nfc</span>
+                    <template v-slot:added>
+                        <span class="mi">radio_button_checked</span>
+                    </template>
+                </CombinedIcon>
+            </BigMessage>
+            <BigMessage message="Zum Zurückgeben" header="Halte das Gerät an das Lesegerät">
+                <CombinedIcon border-radius="8px" added-padding=".05em">
+                    <span class="mi">nfc</span>
+                    <template v-slot:added>
+                        <span class="mi">devices</span>
+                    </template>
+                </CombinedIcon>
+            </BigMessage>
+        </div>
+    </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import BigMessage from "@/components/BigMessage.vue";
+import CombinedIcon from "@/components/CombinedIcon.vue";
 
-export default Vue.extend({});
+export default Vue.extend({
+    components: { BigMessage, CombinedIcon },
+});
 </script>
+
+<style lang="scss" scoped>
+.big-message:first-of-type {
+    margin-bottom: 157px;
+}
+</style>
