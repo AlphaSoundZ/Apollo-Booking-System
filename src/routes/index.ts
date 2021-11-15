637ca223ed4de6f645505227f6fb5c7e663f53eb
Max Heilmann <support@matix-media.net>
2021-11-15 17:34:45 +0100

    Changed client serving

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,14 @@
 import * as express from "express";
 import UI from "./ui";
+import * as path from "path";
 
 const route = express.Router();
 
 route.ws("/ws/ui", UI);
 
-// Serve UI
-route.get("/", express.static("../../client/dist"));
+// Serve Client
+const clientLocation = path.join(__dirname, "../../client/dist");
+console.log(clientLocation);
+route.use(express.static(clientLocation));
 
 export default route;
