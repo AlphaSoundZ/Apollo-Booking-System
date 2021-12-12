41554f2a23aea8cc9436b47c48e0903b88f3d7ea
Max Heilmann <support@matix-media.net>
2021-12-12 18:45:55 +0100

    Added read cycle for rfid reader

--- src/RFIDManager.ts
+++ src/RFIDManager.ts
@@ -1,11 +1,66 @@
 import WebSocketManager from "./lib/WebSocketManager";
+import SoftSPI from "rpi-softspi";
+import Mfrc522 from "mfrc522-rpi";
+import logger from "./config/logger";
 
 export default class RFIDManager {
     private webSockets: WebSocketManager[];
     private apiUrl: string;
+    private reader: Mfrc522;
+    private listening = true;
 
     constructor(apiUrl: string) {
         this.apiUrl = apiUrl;
+
+        const softSPI = new SoftSPI({
+            clock: 23,
+            mosi: 19,
+            miso: 21,
+            client: 24,
+        });
+        this.reader = new Mfrc522(softSPI).setResetPin(22);
+
+        setTimeout(this.readCycle, 250);
+    }
+
+    readCycle() {
+        if (!this.listening) return;
+
+        this.reader.reset();
+
+        let response = this.reader.findCard();
+        if (!response) return;
+
+        logger.debug("RFID detected. CardType: " + response.bitSize);
+
+        response = this.reader.getUid();
+        if (!response.status) {
+            logger.warn("UID scan error");
+            return;
+        }
+
+        const uid = response.data;
+        logger.debug(
+            "Card read UID:",
+            uid[0].toString(16),
+            uid[1].toString(16),
+            uid[2].toString(16),
+            uid[3].toString(16),
+        );
+
+        const memoryCapacity = this.reader.selectCard(uid);
+        logger.debug("Card memory capacity:", memoryCapacity);
+
+        const key = [0xff, 0xff, 0xff, 0xff, 0xff, 0xff];
+
+        if (!this.reader.authenticate(8, key, uid)) {
+            logger.warn("Authentication Error");
+            return;
+        }
+
+        logger.debug("Block: 8 Data:", this.reader.getDataForBlock(8));
+
+        this.reader.stopCrypto();
     }
 
     addWebSocket(ws: WebSocketManager) {
