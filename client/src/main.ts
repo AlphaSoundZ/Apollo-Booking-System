60c533ee1cd06a13da659df82689302fcd1a6644
Max Heilmann <support@matix-media.net>
2021-12-13 00:13:56 +0100

    Added event logic

--- client/src/main.ts
+++ client/src/main.ts
@@ -10,29 +10,74 @@ new Vue({
     router,
     render: (h) => h(App),
     data: {
-        websocket: null
-    } as { websocket: null | WebSocket },
+        websocket: null,
+        pingTimeout: 0
+    } as { websocket: null | WebSocket, pingTimeout: number },
     mounted() {
-        const devMode = process.env.NODE_ENV === 'development';
+        this.websocketSetup();
+    },
+    methods: {
+        websocketSetup() {
+            this.navPage("Loading", {message: "Verbinde mit Server"})
 
-        let url;
-        if (devMode) {
-            url = "ws://192.168.0.93:8080/ws/ui";
-        } else {
-            url = "ws://" + window.location.host + "/ws/ui";
+            const devMode = process.env.NODE_ENV === 'development';
+
+            let url: string;
+            if (devMode) {
+                url = "ws://192.168.0.93:8080/ws/ui";
+            } else {
+                url = "ws://" + window.location.host + "/ws/ui";
+            }
+            
+            console.log("Opening websocket on:", url);
+            this.websocket = new WebSocket(url);
+
+            this.websocket.onopen = () => {
+                this.navPage("Waiting")
+                console.log("Websocket connected");
+                setInterval(() => {
+                    this.websocket?.send(JSON.stringify({type: "event", event: "ping"}));
+                    this.pingTimeout = setTimeout(() => {
+                        console.log("Websocket connection lost! Trying to reconnect");
+                        this.websocketSetup();
+                    }, 3000);
+                }, 15000);
+            }
+
+            this.websocket.onclose = (ev) => {
+                console.log("Connection lost:", ev.reason, "Reconnecting");
+                this.websocketSetup();
+            }
+
+            this.websocket.onmessage = (ev) => {
+                const res = JSON.parse(ev.data)
+                if (res.type == "response" && res.to == "ping") {
+                    clearTimeout(this.pingTimeout);
+                    return;
+                }
+
+                if (res.type == "event") {
+                    switch (res.event) {
+                        case "gettingChipInfo":
+                            this.navPage("Loading", {message: "Lade Daten"})
+                            break;
+                        case "userLogout":
+                            this.navPage("Waiting");
+                            break;
+                        case "userInfo":
+                            this.navPage("userInfo", {username: res.data.user.vorname + res.data.user.nachname, history: res.data.user.history})
+                            break;
+                        case "error":
+                            this.navPage("Error", {message: res.data.message})
+                            break;
+                    }
+                }
+
+                console.log(JSON.parse(ev.data))
+            }
+        },
+        navPage(pageName: string, data: any = {}) {
+            this.$router.push({name: pageName, params: data});
         }
-        
-        console.log("Opening websocket on:", url);
-        this.websocket = new WebSocket(url);
-
-        this.websocket.onopen = () => {
-            console.log("Websocket connected");
-            this.websocket?.send(JSON.stringify({type: "event", event: "ping"}))
-        }
-
-        this.websocket.onmessage = (ev) => {
-            console.log(JSON.parse(ev.data))
-        }
-
     }
 }).$mount("#app");
