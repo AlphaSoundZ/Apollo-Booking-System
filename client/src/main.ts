7a6d8b1f132f15ef2c5c3b6e1e50e196f28aef6a
Max Heilmann <support@matix-media.net>
2021-12-12 23:25:34 +0100

    Added warning supression

--- client/src/main.ts
+++ client/src/main.ts
@@ -9,4 +9,30 @@ Vue.config.productionTip = false;
 new Vue({
     router,
     render: (h) => h(App),
+    data: {
+        websocket: null
+    } as { websocket: null | WebSocket },
+    mounted() {
+        const devMode = process.env.NODE_ENV === 'development';
+
+        let url;
+        if (devMode) {
+            url = "ws://192.168.0.93:8080/ws/ui";
+        } else {
+            url = "ws://" + window.location.host + "/ws/ui";
+        }
+        
+        console.log("Opening websocket on:", url);
+        this.websocket = new WebSocket(url);
+
+        this.websocket.onopen = () => {
+            console.log("Websocket connected");
+            this.websocket?.send(JSON.stringify({type: "event", event: "ping"}))
+        }
+
+        this.websocket.onmessage = (ev) => {
+            console.log(JSON.parse(ev.data))
+        }
+
+    }
 }).$mount("#app");
