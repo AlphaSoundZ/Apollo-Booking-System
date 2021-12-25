6e0df2093d273f12e318cb41d99997dec5c7d699
Max Heilmann <support@matix-media.net>
2021-12-25 20:15:15 +0100

    Added docs

--- src/routes/ui/ping.ts
+++ src/routes/ui/ping.ts
@@ -1,5 +1,7 @@
 import { WSEventListener } from "../../lib/WebSocketManager";
 
+// Ping event
 export default new WSEventListener("ping", async (event) => {
+    // Respond to keep connection alive
     event.respond(true);
 });
