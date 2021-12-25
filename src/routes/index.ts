49e38366e2a6d2b5998b056032d9345c004423b8
Max Heilmann <support@matix-media.net>
2021-12-25 20:13:55 +0100

    Added docs

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,11 +4,14 @@ import * as path from "path";
 
 const route = express.Router();
 
+// Create WebSocket endpoint
 route.ws("/ws/ui", UI);
 
 // Serve Client
 const clientLocation = path.join(__dirname, "../../client/dist");
 console.log(clientLocation);
+
+// Add static UI endpoint (WARNING: UI first has to be build by navigating in the client directory and executing yarn build)
 route.use(express.static(clientLocation));
 
 export default route;
