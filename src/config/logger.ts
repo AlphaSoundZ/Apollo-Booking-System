4e06b7876b1dda023d5a3c125a6c5d41c0cd3c92
Max Heilmann <support@matix-media.net>
2021-12-25 20:09:11 +0100

    Added docs

--- src/config/logger.ts
+++ src/config/logger.ts
@@ -1,11 +1,14 @@
 import * as log4js from "log4js";
 import * as path from "path";
 
+// Check for development environment
 const devMode = (process.env.NODE_ENV || "development") == "development";
 
+// Get logs path and log level
 const logPath = path.join(__dirname, "../../", "logs");
 const logLevel = devMode ? "debug" : "info";
 
+// Configure log4js (log4shell does not exists)
 log4js.configure({
     appenders: {
         log: {
