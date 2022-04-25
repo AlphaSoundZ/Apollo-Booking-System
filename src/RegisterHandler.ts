ade4d4d1bfacd485ccda04862cd81cd2334ac61a
Matix-Media <support@matix-media.net>
2022-04-25 18:43:02 +0200

    Added registerHandler

---
+++ src/RegisterHandler.ts
@@ -0,0 +1,56 @@
+import * as log4js from "log4js";
+import { createInterface as createReadlineInterface } from "readline";
+import Handler from "./Handler";
+import API from "./lib/API";
+import { DeviceType } from "./lib/API";
+
+const logger = log4js.getLogger("cli");
+const readline = createReadlineInterface(process.stdin, process.stdout);
+const token = process.env.REGISTER_TOKEN;
+
+export default class RegisterHandler implements Handler {
+    private static lastSelectedType: DeviceType = DeviceType.IPad;
+
+    public uid: string | null = null;
+    private api: API;
+
+    constructor(api: API) {
+        this.api = api;
+    }
+
+    public async run(uid: string) {
+        logger.debug("[[ Device Types ]]");
+        for (const deviceType of DeviceType.deviceTypes)
+            logger.info("    " + deviceType.id + ": " + deviceType.name);
+
+        const result = await new Promise<string>((resolve) => {
+            readline.question(
+                'Please enter device type for "' +
+                    uid +
+                    '" (or return for ' +
+                    RegisterHandler.lastSelectedType.name +
+                    ")",
+                (answer) => {
+                    resolve(answer);
+                },
+            );
+        });
+        let selectedType = DeviceType.IPad;
+        if (result.length > 0)
+            try {
+                const raw = Number(result);
+                const temp = DeviceType.getById(raw);
+                if (temp != null) selectedType = temp;
+            } catch (_) {}
+
+        RegisterHandler.lastSelectedType = selectedType;
+
+        logger.info('Registering "' + uid + '" as ' + selectedType.name + "...");
+
+        this.register(uid, selectedType);
+    }
+
+    private async register(uid: string, type: DeviceType) {
+        return await this.api.registerDevice(uid, type, token);
+    }
+}
