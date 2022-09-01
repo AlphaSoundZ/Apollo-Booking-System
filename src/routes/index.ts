c0c8858aeafaca1f516f28c6408709af001c11fb
Matix-Media <support@matix-media.net>
2022-09-01 17:32:39 +0200

    fix(reader): :mute: removed console log

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -9,7 +9,6 @@ route.ws("/ws/ui", UI);
 
 // Serve Client
 const clientLocation = path.join(__dirname, "../../client/dist");
-console.log(clientLocation);
 
 // Add static UI endpoint (WARNING: UI first has to be build by navigating in the client directory and executing yarn build)
 route.use(express.static(clientLocation));
